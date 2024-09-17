11e908c3c9d4cc89b2f930b8ea5408e6e9728cc1
Mohammed Ismail Najeeb N <Mohammed.Najeeb@volantetech.com>
2024-09-17 16:45:45 +0530

    Initial commit

---
+++ src/Swing.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Swing {
+//    public Swing(){
+//    }
+
+
+    public static void main(String[] args) {
+        JFrame jFrame=new JFrame();
+        jFrame.setTitle("This is  a normal frame");
+        jFrame.setLayout(null);
+        jFrame.setSize(400,400);
+        jFrame.setVisible(true);
+
+
+    }
+
+}
